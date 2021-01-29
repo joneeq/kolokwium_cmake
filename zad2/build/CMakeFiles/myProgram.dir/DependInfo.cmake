8a5e0f68005053722917518eb2f09266dffa8374
joneeq <johny.sier@gmail.com>
2021-01-29 08:35:28 +0100

    workin zad2 cmake

---
+++ zad2/build/CMakeFiles/myProgram.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Jonasz/dev/Uczelnia/Srodowisko/kolokwium_cmake/zad2/myProgram/address.cpp" "/Users/Jonasz/dev/Uczelnia/Srodowisko/kolokwium_cmake/zad2/build/CMakeFiles/myProgram.dir/address.cpp.o"
+  "/Users/Jonasz/dev/Uczelnia/Srodowisko/kolokwium_cmake/zad2/myProgram/employee.cpp" "/Users/Jonasz/dev/Uczelnia/Srodowisko/kolokwium_cmake/zad2/build/CMakeFiles/myProgram.dir/employee.cpp.o"
+  "/Users/Jonasz/dev/Uczelnia/Srodowisko/kolokwium_cmake/zad2/myProgram/main.cpp" "/Users/Jonasz/dev/Uczelnia/Srodowisko/kolokwium_cmake/zad2/build/CMakeFiles/myProgram.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
