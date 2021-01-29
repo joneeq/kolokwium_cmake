779e96c2d7cbc36cf3b58dbea0e86627421711c8
joneeq <johny.sier@gmail.com>
2021-01-29 09:26:03 +0100

    doesnt work properly

---
+++ zad3/CMakeFiles/Student.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Student.dir/src/Student.cpp.o"
+  "lib/libStudent.a"
+  "lib/libStudent.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Student.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
